afa3e4de5e505adc8f7a0f5cbe5d5dfa22e29385
Emma Gouti <goutiemma@gmail.com>
2018-10-07 04:34:01 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,47 @@
-# Write your code here.
+def dictionary 
+  wordToBeSub = {
+     "hello" => 'hi',
+     "to" => "2",
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+     "four"=> '4',
+     "be"=> 'b',
+     "you"=> 'u',
+     "at"=> '@',
+     "and"=> '&'
+  }
+end
+
+def word_substituter(tw)
+  tw.split.collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  end.join(" ")
+end
+
+
+def bulk_tweet_shortener(array)
+  array.each do |words|
+    puts word_substituter(words)
+end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweets)
+  if word_substituter(tweets).length > 140
+    word_substituter(tweets)[0...137] + "..."
+  else
+    tweets
+  end
+end
